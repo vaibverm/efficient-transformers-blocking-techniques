quic/efficient-transformers

Added support for Qwen2.5_VL PagedAttention
Signed-off-by: Vaibhav Verma <vaibverm@qti.qualcomm.com>

QEfficient/transformers/models/qwen2_5_vl/modeling_qwen2_5_vl.py
@@ -713,6 +713,7 @@ class QEffQwen_2_5_vl_EncoderWrapper(nn.Module):
         super().__init__()
         self.model = model
         self.model.vision_model = self.model.visual
+        self.config = model.config
 
     def get_submodules_for_export(self) -> Type[nn.Module]:
         """
@@ -737,6 +738,7 @@ class QEffQwen_2_5_vl_DecoderWrapper(nn.Module):
         super().__init__()
         self.model = model
         self.language_model = self.model.model.language_model
+        self.config = model.config
 
     def get_submodules_for_export(self) -> Type[nn.Module]:
         """
@@ -825,20 +827,9 @@ class QEffQwen_2_5_vl_ForConditionalGeneration(Qwen2_5_VLForConditionalGeneratio
         lang_inputs = {}
         vision_inputs["pixel_values"] = torch.zeros((inputs_shapes["pixel_values"]), dtype=self.config.torch_dtype)
         vision_inputs["image_grid_thw"] = torch.zeros((inputs_shapes["image_grid_thw"]), dtype=torch.int64)
-        lang_inputs["input_ids"] = torch.zeros((inputs_shapes["input_ids"]), dtype=torch.int64)
-        lang_inputs["vision_embeds"] = torch.zeros((inputs_shapes["vision_embeds"]), dtype=self.config.torch_dtype)
-        lang_inputs["position_ids"] = (
-            (
-                torch.arange(constants.ONNX_EXPORT_EXAMPLE_SEQ_LEN, dtype=torch.int64)
-                .view(1, constants.ONNX_EXPORT_EXAMPLE_SEQ_LEN)
-                .repeat(constants.ONNX_EXPORT_EXAMPLE_BATCH_SIZE, 1)
-            )
-            .unsqueeze(0)
-            .repeat(4, 1, 1)
-        )
-        lang_inputs["image_idx"] = torch.zeros((inputs_shapes["image_idx"]), dtype=torch.int64)
 
         bs: int = constants.ONNX_EXPORT_EXAMPLE_BATCH_SIZE
+        seq_len: int = constants.ONNX_EXPORT_EXAMPLE_SEQ_LEN
         fbs: int = constants.ONNX_EXPORT_EXAMPLE_FBS
 
         # Add data for KV
@@ -847,6 +838,31 @@ class QEffQwen_2_5_vl_ForConditionalGeneration(Qwen2_5_VLForConditionalGeneratio
             batch_size=fbs if continuous_batching else bs,
             seq_len=constants.ONNX_EXPORT_EXAMPLE_SEQ_LEN,
         )
+
+        lang_inputs["input_ids"] = torch.zeros((inputs_shapes["input_ids"]), dtype=torch.int64)
+        qaic_config = getattr(self.model, "qaic_config", None)
+        blocking_mode = qaic_config.get("blocking_mode") if qaic_config is not None else None
+        if blocking_mode is not None and "paged" in blocking_mode:
+            num_kv_blocks = qaic_config["num_kv_blocks"]
+            batch, num_kv_heads, CL, dh = kv_cache_shape
+            seq_len = kv_block_size = -(-CL // num_kv_blocks)
+            total_num_kv_blocks = batch * num_kv_blocks
+            kv_cache_shape = [total_num_kv_blocks, num_kv_heads, kv_block_size, dh]
+            lang_inputs["block_table"] = torch.arange((bs * num_kv_blocks), dtype=torch.int64).view(bs, num_kv_blocks)
+            lang_inputs["slot_id"] = torch.zeros(bs, dtype=torch.int64)
+            lang_inputs["input_ids"] = torch.zeros((bs, seq_len), dtype=torch.int64)
+
+        lang_inputs["vision_embeds"] = torch.zeros((inputs_shapes["vision_embeds"]), dtype=self.config.torch_dtype)
+        lang_inputs["position_ids"] = (
+            (
+                torch.arange(seq_len, dtype=torch.int64)
+                .view(1, seq_len)
+                .repeat(bs, 1)
+            )
+            .unsqueeze(0)
+            .repeat(4, 1, 1)
+        )
+        lang_inputs["image_idx"] = torch.zeros((inputs_shapes["image_idx"]), dtype=torch.int64)
 
         lang_inputs["past_key_values"] = [[] for _ in range(self.model.config.text_config.num_hidden_layers)]
         for i in range(self.model.config.text_config.num_hidden_layers):
@@ -1021,6 +1037,14 @@ class QEffQwen_2_5_vl_ForConditionalGeneration(Qwen2_5_VLForConditionalGeneratio
             if full_batch_size:
                 lang_prefill["full_batch_exec_size"] = full_batch_size
 
+            qaic_config = getattr(self.model, "qaic_config", None)
+            blocking_mode = qaic_config.get("blocking_mode") if qaic_config is not None else None
+            if blocking_mode is not None and "paged" in blocking_mode:
+                num_kv_blocks = self.model.qaic_config["num_kv_blocks"]
+                lang_prefill["num_kv_blocks"] = num_kv_blocks
+                lang_prefill["total_num_kv_blocks"] = kv_cache_batch_size * num_kv_blocks
+                lang_prefill["kv_block_size"] = -(-ctx_len // num_kv_blocks)
+
             lang_decode = {
                 "batch_size": full_batch_size if continuous_batching else batch_size,
                 "seq_len": 1,
@@ -1033,6 +1057,14 @@ class QEffQwen_2_5_vl_ForConditionalGeneration(Qwen2_5_VLForConditionalGeneratio
                 lang_decode["full_batch_size"] = kv_cache_batch_size
             else:
                 lang_decode["batch_size"] = kv_cache_batch_size
+
+            qaic_config = getattr(self.model, "qaic_config", None)
+            blocking_mode = qaic_config.get("blocking_mode") if qaic_config is not None else None
+            if blocking_mode is not None and "paged" in blocking_mode:
+                num_kv_blocks = self.model.qaic_config["num_kv_blocks"]
+                lang_decode["num_kv_blocks"] = num_kv_blocks
+                lang_decode["total_num_kv_blocks"] = kv_cache_batch_size * num_kv_blocks
+                lang_decode["kv_block_size"] = -(-ctx_len // num_kv_blocks)
 
             lang = [lang_prefill, lang_decode]
 
@@ -1064,15 +1096,24 @@ class QEffQwen_2_5_vl_ForConditionalGeneration(Qwen2_5_VLForConditionalGeneratio
             "vision_embeds": {0: "vision_batch_size", 1: "vision_size"},
         }
 
+        pkv_dynamic_axes = {
+                0: "full_batch_size" if continuous_batching else "batch_size",
+                2: "ctx_len",
+            }
+
+        qaic_config = getattr(self.model, "qaic_config", None)
+        blocking_mode = qaic_config.get("blocking_mode") if qaic_config is not None else None
+        if blocking_mode is not None and "paged" in blocking_mode:
+            lang_dynamic_axes["block_table"] = {0: "batch_size", 1: "num_kv_blocks"}
+            lang_dynamic_axes["slot_id"] = {0: "batch_size"}
+            pkv_dynamic_axes = {
+                0: "total_num_kv_blocks",
+                2: "kv_block_size",
+            }
+
         for i in range(num_layers):
-            lang_dynamic_axes[f"past_key.{i}"] = {
-                0: "full_batch_size" if continuous_batching else "batch_size",
-                2: "ctx_len",
-            }
-            lang_dynamic_axes[f"past_value.{i}"] = {
-                0: "full_batch_size" if continuous_batching else "batch_size",
-                2: "ctx_len",
-            }
+            lang_dynamic_axes[f"past_key.{i}"] = pkv_dynamic_axes
+            lang_dynamic_axes[f"past_value.{i}"] = pkv_dynamic_axes
 
         if continuous_batching:
             lang_dynamic_axes["batch_index"] = {0: "batch_size"}

vaibverm/efficient-transformers-blocking-techniques

QEfficient/transformers/models/qwen2_5_vl/modeling_qwen2_5_vl.py
@@ -765,6 +765,7 @@ class QEffQwen_2_5_vl_EncoderWrapper(nn.Module):
         super().__init__()
         self.model = model.model
         self.model.vision_model = self.model.visual
+        self.config = model.config
 
     def get_submodules_for_export(self) -> Type[nn.Module]:
         """
@@ -789,6 +790,7 @@ class QEffQwen_2_5_vl_DecoderWrapper(nn.Module):
         super().__init__()
         self.model = model
         self.language_model = self.model.model.language_model
+        self.config = model.config
 
     def get_submodules_for_export(self) -> Type[nn.Module]:
         """
@@ -881,18 +883,6 @@ class QEffQwen_2_5_vl_ForConditionalGeneration(Qwen2_5_VLForConditionalGeneratio
         lang_inputs = {}
         vision_inputs["pixel_values"] = torch.zeros((inputs_shapes["pixel_values"]), dtype=self.config.torch_dtype)
         vision_inputs["image_grid_thw"] = torch.zeros((inputs_shapes["image_grid_thw"]), dtype=torch.int64)
-        lang_inputs["input_ids"] = torch.zeros((inputs_shapes["input_ids"]), dtype=torch.int64)
-        lang_inputs["vision_embeds"] = torch.zeros((inputs_shapes["vision_embeds"]), dtype=self.config.torch_dtype)
-        lang_inputs["position_ids"] = (
-            (
-                torch.arange(prefill_seq_len, dtype=torch.int64)
-                .view(1, prefill_seq_len)
-                .repeat(constants.ONNX_EXPORT_EXAMPLE_BATCH_SIZE, 1)
-            )
-            .unsqueeze(0)
-            .repeat(4, 1, 1)
-        )
-        lang_inputs["image_idx"] = torch.zeros((inputs_shapes["image_idx"]), dtype=torch.int64)
 
         bs: int = constants.ONNX_EXPORT_EXAMPLE_BATCH_SIZE
         fbs: int = constants.ONNX_EXPORT_EXAMPLE_FBS
@@ -903,6 +893,31 @@ class QEffQwen_2_5_vl_ForConditionalGeneration(Qwen2_5_VLForConditionalGeneratio
             batch_size=fbs if continuous_batching else bs,
             seq_len=prefill_seq_len,
         )
+
+        lang_inputs["input_ids"] = torch.zeros((inputs_shapes["input_ids"]), dtype=torch.int64)
+        qaic_config = getattr(self.model, "qaic_config", None)
+        blocking_mode = qaic_config.get("blocking_mode") if qaic_config is not None else None
+        if blocking_mode is not None and "paged" in blocking_mode:
+            num_kv_blocks = qaic_config["num_kv_blocks"]
+            batch, num_kv_heads, CL, dh = kv_cache_shape
+            prefill_seq_len = kv_block_size = -(-CL // num_kv_blocks)
+            total_num_kv_blocks = batch * num_kv_blocks
+            kv_cache_shape = [total_num_kv_blocks, num_kv_heads, kv_block_size, dh]
+            lang_inputs["block_table"] = torch.arange((bs * num_kv_blocks), dtype=torch.int64).view(bs, num_kv_blocks)
+            lang_inputs["slot_id"] = torch.zeros(bs, dtype=torch.int64)
+            lang_inputs["input_ids"] = torch.zeros((bs, prefill_seq_len), dtype=torch.int64)
+
+        lang_inputs["vision_embeds"] = torch.zeros((inputs_shapes["vision_embeds"]), dtype=self.config.torch_dtype)
+        lang_inputs["position_ids"] = (
+            (
+                torch.arange(prefill_seq_len, dtype=torch.int64)
+                .view(1, prefill_seq_len)
+                .repeat(bs, 1)
+            )
+            .unsqueeze(0)
+            .repeat(4, 1, 1)
+        )
+        lang_inputs["image_idx"] = torch.zeros((inputs_shapes["image_idx"]), dtype=torch.int64)
 
         lang_inputs["past_key_values"] = [[] for _ in range(self.model.config.text_config.num_hidden_layers)]
         for i in range(self.model.config.text_config.num_hidden_layers):
@@ -1067,6 +1082,14 @@ class QEffQwen_2_5_vl_ForConditionalGeneration(Qwen2_5_VLForConditionalGeneratio
             if full_batch_size:
                 lang_prefill["full_batch_exec_size"] = full_batch_size
 
+            qaic_config = getattr(self.model, "qaic_config", None)
+            blocking_mode = qaic_config.get("blocking_mode") if qaic_config is not None else None
+            if blocking_mode is not None and "paged" in blocking_mode:
+                num_kv_blocks = self.model.qaic_config["num_kv_blocks"]
+                lang_prefill["num_kv_blocks"] = num_kv_blocks
+                lang_prefill["total_num_kv_blocks"] = kv_cache_batch_size * num_kv_blocks
+                lang_prefill["kv_block_size"] = -(-ctx_len // num_kv_blocks)
+
             lang_decode = {
                 "batch_size": full_batch_size if continuous_batching else batch_size,
                 "seq_len": 1,
@@ -1079,6 +1102,14 @@ class QEffQwen_2_5_vl_ForConditionalGeneration(Qwen2_5_VLForConditionalGeneratio
                 lang_decode["full_batch_size"] = kv_cache_batch_size
             else:
                 lang_decode["batch_size"] = kv_cache_batch_size
+
+            qaic_config = getattr(self.model, "qaic_config", None)
+            blocking_mode = qaic_config.get("blocking_mode") if qaic_config is not None else None
+            if blocking_mode is not None and "paged" in blocking_mode:
+                num_kv_blocks = self.model.qaic_config["num_kv_blocks"]
+                lang_decode["num_kv_blocks"] = num_kv_blocks
+                lang_decode["total_num_kv_blocks"] = kv_cache_batch_size * num_kv_blocks
+                lang_decode["kv_block_size"] = -(-ctx_len // num_kv_blocks)
 
             lang = [lang_prefill, lang_decode]
 
@@ -1110,15 +1141,24 @@ class QEffQwen_2_5_vl_ForConditionalGeneration(Qwen2_5_VLForConditionalGeneratio
             "vision_embeds": {0: "vision_batch_size", 1: "vision_size"},
         }
 
+        pkv_dynamic_axes = {
+                0: "full_batch_size" if continuous_batching else "batch_size",
+                2: "ctx_len",
+            }
+
+        qaic_config = getattr(self.model, "qaic_config", None)
+        blocking_mode = qaic_config.get("blocking_mode") if qaic_config is not None else None
+        if blocking_mode is not None and "paged" in blocking_mode:
+            lang_dynamic_axes["block_table"] = {0: "batch_size", 1: "num_kv_blocks"}
+            lang_dynamic_axes["slot_id"] = {0: "batch_size"}
+            pkv_dynamic_axes = {
+                0: "total_num_kv_blocks",
+                2: "kv_block_size",
+            }
+
         for i in range(num_layers):
-            lang_dynamic_axes[f"past_key.{i}"] = {
-                0: "full_batch_size" if continuous_batching else "batch_size",
-                2: "ctx_len",
-            }
-            lang_dynamic_axes[f"past_value.{i}"] = {
-                0: "full_batch_size" if continuous_batching else "batch_size",
-                2: "ctx_len",
-            }
+            lang_dynamic_axes[f"past_key.{i}"] = pkv_dynamic_axes
+            lang_dynamic_axes[f"past_value.{i}"] = pkv_dynamic_axes
 
         if continuous_batching:
             lang_dynamic_axes["batch_index"] = {0: "batch_size"}